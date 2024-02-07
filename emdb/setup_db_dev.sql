4ec720838d2a83027dfd85526b092a4771ec39be
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-07 18:55:52 +0300

    database setup

---
+++ emdb/setup_db_dev.sql
@@ -0,0 +1,7 @@
+-- Create database emdb_db
+CREATE DATABASE IF NOT EXISTS emdb_db;
+USE emdb_db;
+CREATE USER IF NOT EXISTS 'emdb_dev'@'localhost' IDENTIFIED BY 'emdb_dev_pwd';
+GRANT ALL PRIVILEGES ON 'emdb_db'.* TO 'emdb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'emdb_dev'@'localhost';
+FLUSH PRIVILEGES;
