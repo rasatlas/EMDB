5f8883816aeb0ef343ee440c99ee1d8ee51b00fe
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-09 18:43:27 +0300

    Association model between movie and people

---
+++ emdb/models/moviepeople.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sets up association class between movie and people"""
+from base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
+
+
+class MoviePeople(Base, BaseModel):
+    """Representation of MoviePeople"""
+    __tablename__ = 'tbl_movie_people'
+
+    movie_id = Column(String(60), ForeignKey('tbl_movie.id'), nullable=False)
+    people_id = Column(String(60), ForeignKey('tbl_people.id'), nullable=False)
+
+    def __init__(self):
+        """Initialization of MoviePeople"""
+        super().__init__()
