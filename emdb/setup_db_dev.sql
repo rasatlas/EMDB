e3448ee37deddebca476ddac18ac41d9b7e4962b
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-14 18:07:39 +0300

    Database and access privileges setup.

--- emdb/setup_db_dev.sql
+++ emdb/setup_db_dev.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS emdb_db;
 USE emdb_db;
 CREATE USER IF NOT EXISTS 'emdb_dev'@'localhost' IDENTIFIED BY 'emdb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'emdb_db'.* TO 'emdb_dev'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'emdb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `emdb_db`.* TO 'emdb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'emdb_dev'@'localhost';
 FLUSH PRIVILEGES;
