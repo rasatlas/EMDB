88d25434a7bed261d82219d4cc3311fe8cfb68d8
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-15 16:49:08 +0300

    Storage engine load at Python package initialization.

--- emdb/models/__init__.py
+++ emdb/models/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Create an instance of StoregeEngine and import all modesls,
+""" Create an instance of StoregeEngine and import all modesls,
     so, database is ready at system startup.
 """
 
-from models.base_model import BaseModel
 from models.engine.storageengine import StorageEngine
 
 
