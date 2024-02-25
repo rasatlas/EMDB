1cf5db8f236cdad792acf8285d1139421bb15f39
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-25 03:00:32 +0300

    Forms setup.

---
+++ emdb/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, FileField
+from wtforms.validators import DataRequired
+
+
+class ActorForm(FlaskForm):
+    FirstName = StringField("First Name", validators=[DataRequired()])
+    FatherName = StringField("Father Name", validators=[DataRequired()])
+    GrandFatherName = StringField("Grand Father Name")
+    Height = StringField("Height")
+    HeadShot = FileField("Head Shot", validators=[DataRequired()])
+    BirthDate = DateField("Birth Date")
+    DeathDate = DateField("Death Date")
+    Submit = SubmitField("Save")
+
+
+class Language(FlaskForm):
+    Language = StringField("Language", validators=[DataRequired()])
+
+
+class Genre(FlaskForm):
+    Genre = StringField("Genre", validators=[DataRequired()])
+
+
+class PGRating(FlaskForm):
+    PGRating = StringField("PG Rating", validators=[DataRequired()])
