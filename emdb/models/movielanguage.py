2e291e84c1101ef0a2ae23082d3d9dc8f7833a00
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-28 18:08:41 +0300

    Relationship definition and import fix.

--- emdb/models/movielanguage.py
+++ emdb/models/movielanguage.py
@@ -2,6 +2,7 @@
 """Sets up association class between movie and language."""
 from .base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class MovieLanguage(BaseModel, Base):
@@ -11,6 +12,10 @@ class MovieLanguage(BaseModel, Base):
     movie_id = Column(String(60), ForeignKey('tbl_movie.id'), nullable=False)
     language_id = Column(String(60), ForeignKey('tbl_language.id'),
                          nullable=False)
+
+    # Define relationship
+    movie = relationship("Movie", backref='language')
+    language = relationship("Language", backref='movie')
 
     def __init__(self):
         """Initialization of MovieLanguage"""
