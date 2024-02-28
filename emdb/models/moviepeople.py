94c906ea2fb0b3acdf6a7169e408fe35dbe3cae6
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-02-28 18:09:05 +0300

    Relationship definition and import fix.

--- emdb/models/moviepeople.py
+++ emdb/models/moviepeople.py
@@ -2,6 +2,7 @@
 """Sets up association class between movie and people."""
 from .base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class MoviePeople(BaseModel, Base):
@@ -10,7 +11,11 @@ class MoviePeople(BaseModel, Base):
 
     movie_id = Column(String(60), ForeignKey('tbl_movie.id'), nullable=False)
     people_id = Column(String(60), ForeignKey('tbl_people.id'), nullable=False)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=True)
+
+    # Defining relationship.
+    people = relationship("People", backref='movie')
+    movie = relationship("Movie", backref='people')
 
     def __init__(self):
         """Initialization of MoviePeople"""
